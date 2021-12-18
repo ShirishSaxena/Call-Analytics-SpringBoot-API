13a7d929cffc20272979540281f013aad7d03381
Shirish Saxena <6762915+ShirishSaxena@users.noreply.github.com>
2021-12-19 01:30:37 +0530

    Update Generate Random POST Requests.py

--- Generate Random POST Requests.py
+++ Generate Random POST Requests.py
@@ -4,7 +4,7 @@ import pyperclip
 from datetime import datetime
 
 # Random number generator for Adjetter Media assignment (Java)
-numToGenerate = 5
+numToGenerate = 100000
 mobileNoStart = 9
 
 # seconds
@@ -18,8 +18,8 @@ randomDate_start = "01-01-2021 00:00:00"
 randomDate_end = "31-12-2021 23:00:00"
 
 def getRandomNum():
-    res = [random.randrange(0,9, 1) for i in range(9)]
-    res.insert(0,mobileNoStart);
+    res = [random.randint(0,9) for i in range(9)]
+    res.insert(0, mobileNoStart);
     return sum(d * 10**i for i, d in enumerate(res[::-1]))
 
 def getRandomTime():
@@ -52,6 +52,3 @@ for i in range(0, numToGenerate):
 result = result[ : -1] + "\n]"
 pyperclip.copy(result)
 print(result)
-
-
-
