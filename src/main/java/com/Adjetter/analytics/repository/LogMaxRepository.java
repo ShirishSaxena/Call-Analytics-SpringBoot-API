c379535eb8be922968a7322353a0af536c095b98
Shirish Saxena <6762915+ShirishSaxena@users.noreply.github.com>
2021-12-20 13:17:55 +0530

    Update LogMaxRepository.java

--- src/main/java/com/Adjetter/analytics/repository/LogMaxRepository.java
+++ src/main/java/com/Adjetter/analytics/repository/LogMaxRepository.java
@@ -8,17 +8,19 @@ import java.sql.Date;
 import java.util.List;
 
 public interface LogMaxRepository extends JpaRepository<LogMax, Long> {
-    LogMax findByDate(Date date);
+    LogMax findByDate(String date);
 
     LogMax findByYearAndWeekNumber(Integer year, Integer weekNumber);
 
     @Query(value = "SELECT DAYNAME(:date)", nativeQuery = true)
     String getDayNameFromDate(Date date);
 
-    @Query("SELECT l.dayName, l.totalDuration, l.date FROM LogMax l WHERE l.totalDuration = (SELECT MAX(n.totalDuration) FROM LogMax n WHERE n.year = :year AND n.weekNumber = :weekNumber) AND l.year = :year AND l.weekNumber = :weekNumber")
-    List<Object> getMaxVolumeFromWeek(Integer year, Integer weekNumber);
+    //@Query("SELECT dayName, totalDuration, date FROM LogMax INNER JOIN (Select MAX(totalDuration) as totalDuration from LogMax WHERE year=:year AND weekNumber = :weekNumber) as max USING(totalDuration)")
+    @Query("SELECT l FROM LogMax l WHERE l.totalDuration = (SELECT MAX(n.totalDuration) FROM LogMax n WHERE n.year = :year AND n.weekNumber = :weekNumber) AND l.year = :year AND l.weekNumber = :weekNumber")
+    List<LogMax> getMaxVolumeFromWeek(Integer year, Integer weekNumber);
 
-    @Query("SELECT l.dayName, l.LongestCall, l.date FROM LogMax l WHERE l.LongestCall = (SELECT MAX(n.LongestCall) FROM LogMax n WHERE n.year = :year AND n.weekNumber = :weekNumber) AND l.year = :year AND l.weekNumber = :weekNumber")
-    List<Object> getLongestCallFromWeek(Integer year, Integer weekNumber);
+    @Query("SELECT l FROM LogMax l WHERE l.year = :year AND l.weekNumber = :weekNumber AND l.LongestCall = (SELECT MAX(n.LongestCall) FROM LogMax n WHERE n.year = :year AND n.weekNumber = :weekNumber)")
+    List<LogMax> getLongestCallFromWeek(Integer year, Integer weekNumber);
+
 
 }
